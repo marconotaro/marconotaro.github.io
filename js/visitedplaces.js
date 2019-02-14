5513f157779f9b56f8f87ee97c50049f6c502b8c
marconotaro <notaromarco90@gmail.com>
2019-02-14 15:22:32 +0100

    relocate leaflet javascript

--- js/visitedplaces.js
+++ js/visitedplaces.js
@@ -1,357 +1,46 @@
-var places=[
-  {
-    "city": "Heidelberg",
-    "lat": 49.4093582,
-    "lon": 8.694724
-  },
-  {
-    "city": "Worms",
-    "lat": 49.6352685,
-    "lon": 8.3603088
-  },
-  {
-    "city": "Spira",
-    "lat": 49.3165553,
-    "lon": 8.433615
-  },
-  {
-    "city": "Bad-Wimpfen",
-    "lat": 49.2302875,
-    "lon": 9.1621599
-  },
-  {
-    "city": "Baden-Baden",
-    "lat": 48.7580777,
-    "lon": 8.2415239
-  },
-  {
-    "city": "Berlin",
-    "lat": 52.5170365,
-    "lon": 13.3888599
-  },
-  {
-    "city": "Potsdam",
-    "lat": 52.4009309,
-    "lon": 13.0591397
-  },
-  {
-    "city": "Trier",
-    "lat": 49.7596208,
-    "lon": 6.6441878
-  },
-  {
-    "city": "Paris",
-    "lat": 48.8566101,
-    "lon": 2.3514992
-  },
-  {
-    "city": "Mont Saint Michel",
-    "lat": 48.6358559,
-    "lon": -1.5126096
-  },
-  {
-    "city": "Strasbourg",
-    "lat": 48.584614,
-    "lon": 7.7507127
-  },
-  {
-    "city": "Milan",
-    "lat": 45.4667971,
-    "lon": 9.1904984
-  },
-  {
-    "city": "Arco",
-    "lat": 45.9180319,
-    "lon": 10.8860765
-  },
-  {
-    "city": "Bergamo",
-    "lat": 45.6944947,
-    "lon": 9.6698727
-  },
-  {
-    "city": "Verona",
-    "lat": 45.4384958,
-    "lon": 10.9924122
-  },
-  {
-    "city": "Siena (Italy)",
-    "lat": 43.3185536,
-    "lon": 11.3316533
-  },
-  {
-    "city": "Trento",
-    "lat": 46.0664228,
-    "lon": 11.1257601
-  },
-  {
-    "city": "Florence",
-    "lat": 43.7698712,
-    "lon": 11.2555757
-  },
-  {
-    "city": "Turin",
-    "lat": 45.0677551,
-    "lon": 7.6824892
-  },
-  {
-    "city": "Como",
-    "lat": 45.81082,
-    "lon": 9.086044
-  },
-  {
-    "city": "Roma",
-    "lat": 41.894802,
-    "lon": 12.4853384
-  },
-  {
-    "city": "Venice",
-    "lat": 45.4371908,
-    "lon": 12.3345898
-  },
-  {
-    "city": "Monasterolo del Castello",
-    "lat": 45.763185,
-    "lon": 9.931658
-  },
-  {
-    "city": "Pavia",
-    "lat": 45.1860043,
-    "lon": 9.1546375
-  },
-  {
-    "city": "Sirmione",
-    "lat": 45.4688494,
-    "lon": 10.6078237
-  },
-  {
-    "city": "Cagliari",
-    "lat": 39.2169525,
-    "lon": 9.1128513
-  },
-  {
-    "city": "Costa Rei",
-    "lat": 39.2560813,
-    "lon": 9.575808
-  },
-  {
-    "city": "Villasimius",
-    "lat": 39.14314,
-    "lon": 9.520854
-  },
-  {
-    "city": "Stilo",
-    "lat": 38.4766124,
-    "lon": 16.4672633
-  },
-  {
-    "city": "Mammola",
-    "lat": 38.363576,
-    "lon": 16.239403
-  },
-  {
-    "city": "Catanzaro",
-    "lat": 38.9076353,
-    "lon": 16.5960465
-  },
-  {
-    "city": "Soverato",
-    "lat": 38.69274545,
-    "lon": 16.5301697196442
-  },
-  {
-    "city": "Lecco",
-    "lat": 45.8553764,
-    "lon": 9.389605
-  },
-  {
-    "city": "Genoa",
-    "lat": 44.40726,
-    "lon": 8.9338624
-  },
-  {
-    "city": "Bolzano",
-    "lat": 46.4981125,
-    "lon": 11.3547801
-  },
-  {
-    "city": "Chiavenna",
-    "lat": 46.3199792,
-    "lon": 9.3981218
-  },
-  {
-    "city": "Brunate",
-    "lat": 45.8194702,
-    "lon": 9.0959895
-  },
-  {
-    "city": "Livigno",
-    "lat": 46.5378006,
-    "lon": 10.1363165
-  },
-  {
-    "city": "Garzeno",
-    "lat": 46.1342737,
-    "lon": 9.2483448
-  },
-  {
-    "city": "Bellagio",
-    "lat": 45.9872549,
-    "lon": 9.2613001
-  },
-  {
-    "city": "Varenna",
-    "lat": 46.0099785,
-    "lon": 9.2831593
-  },
-  {
-    "city": "Bellano",
-    "lat": 46.0433967,
-    "lon": 9.3060387
-  },
-  {
-    "city": "Menaggio",
-    "lat": 46.022565,
-    "lon": 9.2414329
-  },
-  {
-    "city": "Monte Isola",
-    "lat": 45.711994,
-    "lon": 10.0822353982026
-  },
-  {
-    "city": "Iseo",
-    "lat": 45.6595554,
-    "lon": 10.0510489
-  },
-  {
-    "city": "Crema",
-    "lat": 45.362828,
-    "lon": 9.6874573
-  },
-  {
-    "city": "Mantua",
-    "lat": 45.1566683,
-    "lon": 10.7917191
-  },
-  {
-    "city": "Capo di Lago",
-    "lat": 45.881563,
-    "lon": 10.1630518
-  },
-  {
-    "city": "Dublin",
-    "lat": 53.3497645,
-    "lon": -6.2602732
-  },
-  {
-    "city": "Cliffs of Moher",
-    "lat": 52.9717877,
-    "lon": -9.4291244
-  },
-  {
-    "city": "Cambridge",
-    "lat": 52.2034823,
-    "lon": 0.1235817
-  },
-  {
-    "city": "Palaiokastritsa",
-    "lat": 39.6757716,
-    "lon": 19.7119035
-  },
-  {
-    "city": "Corfu",
-    "lat": 39.591337,
-    "lon": 19.8596189187339
-  },
-  {
-    "city": "Yaliskari",
-    "lat": 39.5772842,
-    "lon": 19.8259723
-  },
-  {
-    "city": "Sidari (Greece)",
-    "lat": 39.792821,
-    "lon": 19.7017291
-  },
-  {
-    "city": "Sydney",
-    "lat": -33.8548157,
-    "lon": 151.2164539
-  },
-  {
-    "city": "Canberra",
-    "lat": -35.2975906,
-    "lon": 149.1012676
-  },
-  {
-    "city": "Wollongong",
-    "lat": -34.4243941,
-    "lon": 150.89385
-  },
-  {
-    "city": "Blue Mountains National Park",
-    "lat": -33.6190685,
-    "lon": 150.487633990821
-  },
-  {
-    "city": "Cronulla",
-    "lat": -34.0575259,
-    "lon": 151.1523851
-  },
-  {
-    "city": "Zurich",
-    "lat": 47.3769434,
-    "lon": 8.5414061
-  },
-  {
-    "city": "Saint Moritz",
-    "lat": 46.4960592,
-    "lon": 9.8386578
-  },
-  {
-    "city": "Granada (Spain)",
-    "lat": 37.183054,
-    "lon": -3.6021928
-  },
-  {
-    "city": "Cordova",
-    "lat": 37.8849779,
-    "lon": -4.7760131
-  },
-  {
-    "city": "Sevilla",
-    "lat": 37.3886303,
-    "lon": -5.9953403
-  },
-  {
-    "city": "Agaete",
-    "lat": 28.1008499,
-    "lon": -15.7023479
-  },
-  {
-    "city": "Galdar",
-    "lat": 28.1442408,
-    "lon": -15.6542506
-  },
-  {
-    "city": "Maspalomas",
-    "lat": 27.7527731,
-    "lon": -15.5970832
-  },
-  {
-    "city": "Playa de Mogán",
-    "lat": 27.820932,
-    "lon": -15.7614534
-  },
-  {
-    "city": "Roque Nublo",
-    "lat": 27.9708896,
-    "lon": -15.6125928
-  },
-  {
-    "city": "Wieliczka",
-    "lat": 49.9823773,
-    "lon": 20.0602114
-  }
-]
+// script to create an Interactive Map by using Leaflet and OpenStreetMap and clustering the markers with markercluster
+
+// full screen control: https://github.com/brunob/leaflet.fullscreen
+var map = L.map('map',{
+zoomControl: true,
+zoomControlOptions: {position: 'topright'},
+fullscreenControl: true,
+fullscreenControlOptions: {position: 'topright'}
+}).setView([14, 14], 2);
+
+// set map tiles source
+mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+boxLink = '<a href="https://www.mapbox.com/">Mapbox</a>';			
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',{
+	attribution: '&copy; ' + mapLink + ' Contributors,' + ' Imagery ' + boxLink,
+	maxZoom: 20,
+	id: 'mapbox.streets',
+	accessToken: 'pk.eyJ1IjoibW41MTUiLCJhIjoiY2pteDVwNGZtMnF2eTNrcG4xcWp6M2FoeSJ9.IHSYNXCOHQJTSy0eGVEtZw'
+}).addTo(map); 
+
+// custom icon
+// options doc: https://leafletjs.com/reference-1.3.4.html#icon
+// markers link: https://github.com/pointhi/leaflet-color-markers
+var CustomIcon = L.icon({
+	iconUrl: 'img/marker-icon-violet.png', 
+	iconRetinaUrl: "img/marker-icon-2x-violet.png", 
+	iconSize: [15,25], // resize original icon of 60%
+	iconAnchor: [7,24],
+	popupAnchor: [1,-15]
+});
+
+// add a single marker for each city 
+// for (var i = 0; i < places.length; i++) {
+// 	marker = new L.marker([places[i].lat, places[i].lon], {icon: CustomIcon})
+// 	.bindPopup(places[i][0])
+// 	.addTo(map);
+// }
+
+// clusterize marker on the map
+var markerClusters = L.markerClusterGroup();
+for(var i=0; i<places.length; i++){			
+	var popup = places[i].city;
+	var marker = L.marker([places[i].lat, places[i].lon], {icon: CustomIcon}).bindPopup(popup);
+	markerClusters.addLayer(marker);
+}
+map.addLayer(markerClusters);
