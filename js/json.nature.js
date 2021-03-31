27c221efa5b39655b9b8b45e3fcb0d67c70b8ec7
marconotaro <notaromarco90@gmail.com>
2021-03-31 20:21:41 +0200

    add naturemap

---
+++ js/json.nature.js
@@ -0,0 +1,326 @@
+var spots=[
+  {
+    "spot": "Rifugio Mario Merelli al Coca",
+    "lat": 46.0546874,
+    "lon": 10.00986065,
+    "ele": "1,892"
+  },
+  {
+    "spot": "Rifugio Antonio Baroni al Brunone",
+    "lat": 46.0623605,
+    "lon": 9.96067116,
+    "ele": "2,295"
+  },
+  {
+    "spot": "Rifugio Laghi Gemelli",
+    "lat": 45.99301195,
+    "lon": 9.80363177,
+    "ele": "1,968"
+  },
+  {
+    "spot": "Rifugio Antonio Curò",
+    "lat": 46.0618139,
+    "lon": 10.04773136,
+    "ele": "1,915"
+  },
+  {
+    "spot": "Rifugio Alpe Corte",
+    "lat": 45.9577783,
+    "lon": 9.82397779,
+    "ele": "1,410"
+  },
+  {
+    "spot": "Rifugio Barbellino",
+    "lat": 46.076584,
+    "lon": 10.07965359,
+    "ele": "2,130"
+  },
+  {
+    "spot": "Rifugio Cesare Benigni",
+    "lat": 46.02138105,
+    "lon": 9.5619178,
+    "ele": "2,220"
+  },
+  {
+    "spot": "Rifugio Luigi Albani",
+    "lat": 45.96607245,
+    "lon": 10.05599399,
+    "ele": "1,939"
+  },
+  {
+    "spot": "Rifugio Fratelli Calvi",
+    "lat": 46.02352815,
+    "lon": 9.8770142,
+    "ele": "2,015"
+  },
+  {
+    "spot": "Rifugio Gherardi",
+    "lat": 45.9257298,
+    "lon": 9.56483601,
+    "ele": "1,650"
+  },
+  {
+    "spot": "Rifugio Magnolini",
+    "lat": 45.8713508,
+    "lon": 10.08204051,
+    "ele": "1,612"
+  },
+  {
+    "spot": "Rifugio Resegone",
+    "lat": 45.8560235,
+    "lon": 9.48908777,
+    "ele": "1,265"
+  },
+  {
+    "spot": "Rifugio Tagliaferri",
+    "lat": 46.05539045,
+    "lon": 10.11469431,
+    "ele": "2,328"
+  },
+  {
+    "spot": "Pizzo Tre Signori",
+    "lat": 46.011533,
+    "lon": 9.527442,
+    "ele": "2,553"
+  },
+  {
+    "spot": "Lago Branchino",
+    "lat": 45.94978585,
+    "lon": 9.80278956,
+    "ele": "1,787"
+  },
+  {
+    "spot": "Lago Moro",
+    "lat": 45.8800251,
+    "lon": 10.15742345,
+    "ele": null
+  },
+  {
+    "spot": "Lago della Malgina",
+    "lat": 46.08256055,
+    "lon": 10.05945047,
+    "ele": "2,339"
+  },
+  {
+    "spot": "Station Punta Helbronner",
+    "lat": 45.8458717,
+    "lon": 6.9317558,
+    "ele": "3,462"
+  },
+  {
+    "spot": "Rifugio Guido Larcher al Cevedale",
+    "lat": 46.4369195,
+    "lon": 10.6660522,
+    "ele": "2,608"
+  },
+  {
+    "spot": "Passo Forcola",
+    "lat": 46.4492425,
+    "lon": 10.6519884,
+    "ele": "3,032"
+  },
+  {
+    "spot": "Rifugio Nuvolau",
+    "lat": 46.4952253,
+    "lon": 12.04570216,
+    "ele": "2,575"
+  },
+  {
+    "spot": "Rifugio Scoiattoli",
+    "lat": 46.50805945,
+    "lon": 12.0467579,
+    "ele": "2,255"
+  },
+  {
+    "spot": "Costa Rei",
+    "lat": 39.2560813,
+    "lon": 9.575808,
+    "ele": null
+  },
+  {
+    "spot": "Villasimius",
+    "lat": 39.14314,
+    "lon": 9.520854,
+    "ele": null
+  },
+  {
+    "spot": "Lauterbrunnen",
+    "lat": 46.5939043,
+    "lon": 7.9078016,
+    "ele": null
+  },
+  {
+    "spot": "Schilthorn",
+    "lat": 46.5573271,
+    "lon": 7.8352093,
+    "ele": "2,973"
+  },
+  {
+    "spot": "Rheinfall (Switzerland)",
+    "lat": 47.6778348,
+    "lon": 8.6153284,
+    "ele": null
+  },
+  {
+    "spot": "Grosser Aletschgletscher",
+    "lat": 46.45006345,
+    "lon": 8.07747558,
+    "ele": null
+  },
+  {
+    "spot": "Stoss",
+    "lat": 47.3608444,
+    "lon": 9.4948897,
+    "ele": "943"
+  },
+  {
+    "spot": "Rigi",
+    "lat": 47.0567184,
+    "lon": 8.4853157,
+    "ele": "1,798"
+  },
+  {
+    "spot": "Pizolhütte",
+    "lat": 46.9792258,
+    "lon": 9.4170382,
+    "ele": "2,221"
+  },
+  {
+    "spot": "Suls-Lobhornhütte",
+    "lat": 46.6184883,
+    "lon": 7.8689998,
+    "ele": "1,955"
+  },
+  {
+    "spot": "Flims",
+    "lat": 46.8332439,
+    "lon": 9.2834557,
+    "ele": "1,081"
+  },
+  {
+    "spot": "Caumasee",
+    "lat": 46.82021335,
+    "lon": 9.29630272,
+    "ele": "997"
+  },
+  {
+    "spot": "Sardonahütte",
+    "lat": 46.91548975,
+    "lon": 9.2754248,
+    "ele": "2,158"
+  },
+  {
+    "spot": "Lenzerheide",
+    "lat": 46.727896,
+    "lon": 9.5568344,
+    "ele": "1,476"
+  },
+  {
+    "spot": "Innerarosa",
+    "lat": 46.774461,
+    "lon": 9.6597439,
+    "ele": "1,820"
+  },
+  {
+    "spot": "Davos",
+    "lat": 46.7961744,
+    "lon": 9.8237266,
+    "ele": "1,560"
+  },
+  {
+    "spot": "Linthal (Switzerland)",
+    "lat": 46.918987,
+    "lon": 8.99714,
+    "ele": "662"
+  },
+  {
+    "spot": "Muttseehuette",
+    "lat": 46.85747595,
+    "lon": 9.01878839,
+    "ele": "2,501"
+  },
+  {
+    "spot": "Limmerensee",
+    "lat": 46.83461775,
+    "lon": 9.01411512,
+    "ele": null
+  },
+  {
+    "spot": "Oeschinensee",
+    "lat": 46.4984413,
+    "lon": 7.72648598,
+    "ele": "1,578"
+  },
+  {
+    "spot": "Cliffs of Moher",
+    "lat": 52.9717877,
+    "lon": -9.4291244,
+    "ele": null
+  },
+  {
+    "spot": "Cofete",
+    "lat": 28.1023118,
+    "lon": -14.3888189,
+    "ele": null
+  },
+  {
+    "spot": "Parque Nacional de Timanfaya ",
+    "lat": 29.02164345,
+    "lon": -13.78123589,
+    "ele": null
+  },
+  {
+    "spot": "Roque Nublo",
+    "lat": 27.9708896,
+    "lon": -15.6125928,
+    "ele": "1,813"
+  },
+  {
+    "spot": "Dunas de Maspalomas",
+    "lat": 27.743883,
+    "lon": -15.58289927,
+    "ele": null
+  },
+  {
+    "spot": "Parque Natural de Corralejo",
+    "lat": 28.7026411,
+    "lon": -13.83644229,
+    "ele": null
+  },
+  {
+    "spot": "Lobos Island",
+    "lat": 28.7501766,
+    "lon": -13.8217035,
+    "ele": null
+  },
+  {
+    "spot": "Paleokastritsa",
+    "lat": 39.6757716,
+    "lon": 19.7119035,
+    "ele": null
+  },
+  {
+    "spot": "Yaliskari",
+    "lat": 39.5772842,
+    "lon": 19.8259723,
+    "ele": null
+  },
+  {
+    "spot": "Sidari (Corfù)",
+    "lat": 39.792821,
+    "lon": 19.7017291,
+    "ele": null
+  },
+  {
+    "spot": "Blue Mountains National Park",
+    "lat": -33.6190685,
+    "lon": 150.48763399,
+    "ele": null
+  },
+  {
+    "spot": "Stanwell Park",
+    "lat": -34.228928,
+    "lon": 150.9881239,
+    "ele": null
+  }
+]
