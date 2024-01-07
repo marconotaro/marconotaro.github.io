d070a9307d462f62ae6b24e0b2743a1596cd272b
marconotaro <marco.notaro@engene.ch>
2024-01-07 17:58:32 +0100

    remove 1px lines

--- js/leaflet.map.js
+++ js/leaflet.map.js
@@ -4,17 +4,18 @@
   workaround for 1px lines appearing in some browsers due to fractional transforms and resulting anti-aliasing.
   link: https://github.com/Leaflet/Leaflet/issues/3575
 */
-(function(){
-  var originalInitTile = L.GridLayer.prototype._initTile
-  L.GridLayer.include({
-    _initTile: function (tile) {
-      originalInitTile.call(this, tile);
-      var tileSize = this.getTileSize();
-      tile.style.width = tileSize.x + 1 + 'px';
-      tile.style.height = tileSize.y + 1 + 'px';
-    }
-  });
-})()
+// (function(){
+//   var originalInitTile = L.GridLayer.prototype._initTile
+//   L.GridLayer.include({
+//     _initTile: function (tile) {
+//       originalInitTile.call(this, tile);
+//       var tileSize = this.getTileSize();
+//       tile.style.width = tileSize.x + 1 + 'px';
+//       tile.style.height = tileSize.y + 1 + 'px';
+//     }
+//   });
+// })()
+
 
 /*
   custom icon
